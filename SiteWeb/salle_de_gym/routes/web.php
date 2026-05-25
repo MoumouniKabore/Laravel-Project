97f35ac57013e0aa78dbb05f013e842663d3c77f
Mkabore <mkabore.contact@gmail.com>
2026-05-25 14:39:51 +0000

    Ajout des routes pour les différentes pages.

--- SiteWeb/salle_de_gym/routes/web.php
+++ SiteWeb/salle_de_gym/routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Http\Controllers\Pages\PagesController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Quand on visite "/", Inertia rend le composant "Home.vue"
-// et lui passe la variable "message"
-Route::get('/', function () {
-    return Inertia::render('Home', [
-        'message' => 'Bienvenue sur mon application ! 🎉',
-    ]);
-})->name('home'); // ->name() permet d'utiliser route('home') avec Ziggy
+Route::get('/', [PagesController::class, 'acceuil'])->name('acceuil');
+Route::get('about', [PagesController::class, 'about'])->name('about');
+Route::get('session', [PagesController::class, 'session'])->name('session');
+Route::get('service', [PagesController::class, 'service'])->name('service');
+Route::get('team', [PagesController::class, 'team'])->name('team');
+Route::get('gallery', [PagesController::class, 'gallery'])->name('gallery');
+Route::get('bmi', [PagesController::class, 'bmi'])->name('bmi');
+Route::get('contact', [PagesController::class, 'contact'])->name('contact');
+
+Route::fallback(function () {
+    return Inertia::render('Site/Errors/404');
+});
