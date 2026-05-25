9469c6e8ca9634dd4a3ee44e51548e290d8e7830
Mkabore <mkabore.contact@gmail.com>
2026-05-25 14:35:11 +0000

    Création du fichier controller, PagesController.php, pour les différentes pages.

---
+++ SiteWeb/salle_de_gym/app/Http/Controllers/Pages/PagesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+// use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function acceuil(){
+        return Inertia::render('Site/Home');
+    }
+
+    public function about(){
+        return Inertia::render('Site/About');
+    }
+
+    public function session(){
+        return Inertia::render('Site/Session');
+    }
+
+    public function service(){
+        return Inertia::render('Site/Service');
+    }
+
+    public function team(){
+        return Inertia::render('Site/Team');
+    }
+
+    public function gallery(){
+        return Inertia::render('Site/Gallery');
+    }
+
+    public function bmi(){
+        return Inertia::render('Site/Bmi');
+    }
+
+    public function contact(){
+        return Inertia::render('Site/Contact');
+    }
+}
