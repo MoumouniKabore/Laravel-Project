5b3d9e145f6ad1cbefc7af07bde248f43f4cf9aa
Mkabore <mkabore.contact@gmail.com>
2026-05-25 14:56:44 +0000

    Suppression des fichiers welcome.blade.php et Home.vue, des pages de test et légère modification du fichier app.blade.php

--- SiteWeb/salle_de_gym/resources/views/app.blade.php
+++ SiteWeb/salle_de_gym/resources/views/app.blade.php
@@ -4,14 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Mon Application</title>
-
-        {{-- Ziggy : injecte les routes Laravel en JavaScript --}}
         @routes
-
-        {{-- Vite : charge les fichiers CSS et JS compilés --}}
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        {{-- Inertia : gère le <head> dynamiquement (titre, meta, etc.) --}}
         @inertiaHead
     </head>
     <body>
