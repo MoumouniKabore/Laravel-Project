72a45a6616bc7336b3bacd95ff76156e3cd89fd0
Mkabore <mkabore.contact@gmail.com>
2026-05-31 18:35:18 +0000

    Ajout du lien de bootstrap-icons dans le <head> pour l'utilisation des icons

--- SiteWeb/salle_de_gym/resources/views/app.blade.php
+++ SiteWeb/salle_de_gym/resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Mon Application</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
         @routes
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
