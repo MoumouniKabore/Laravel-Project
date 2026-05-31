496d83263e7756198a3d01c5b140dd3ac0b454ba
Mkabore <mkabore.contact@gmail.com>
2026-05-31 21:10:39 +0000

    Ajout du CDN swiper

--- SiteWeb/salle_de_gym/resources/views/app.blade.php
+++ SiteWeb/salle_de_gym/resources/views/app.blade.php
@@ -5,12 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Mon Application</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
         @routes
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
     </head>
     <body>
-        {{-- Inertia : c'est ici que les composants Vue seront rendus --}}
         @inertia
+
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     </body>
 </html>
