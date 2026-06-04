5ab3162f7bc9e91b277a81b97ddcd8ffbc109909
Mkabore <mkabore.contact@gmail.com>
2026-06-04 13:00:59 +0000

    Création du composant 'whychoose'

---
+++ SiteWeb/salle_de_gym/resources/js/Components/WhyChoose.vue
@@ -0,0 +1,51 @@
+<script setup>
+</script>
+
+<template>
+    <section class="bg-black/97 px-14 py-16">
+        <h4 class="uppercase text-center text-xl text-amber-500 font-semibold mb-2">why chose us?</h4>
+        <h3 class="uppercase text-center text-4xl text-white font-bold">push your limits forward</h3>
+        <div class="mt-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div class="group text-center space-y-3">
+                <span class="inline-block bg-gray-900 text-amber-500 text-4xl px-5 py-4 rounded-full group-hover:bg-amber-500 group-hover:text-white transition duration-400">
+                    <i class="bi bi-boxes"></i>
+                </span>
+                <h4 class="text-2xl text-white font-bold">Modern equipment</h4>
+                <p class="text-gray-400 text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sequi 
+                    neque accusamus veritatis.
+                </p>
+            </div>
+            <div class="group text-center space-y-3">
+                <span class="inline-block bg-gray-900 text-amber-500 text-4xl px-5 py-4 rounded-full group-hover:bg-amber-500 group-hover:text-white transition duration-400">
+                    <i class="bi bi-cup-fill"></i>
+                </span>
+                <h4 class="text-2xl text-white font-bold">Healthy nutrition plan</h4>
+                <p class="text-gray-400 text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sequi 
+                    neque accusamus veritatis.
+                </p>
+            </div>
+            <div class="group text-center space-y-3">
+                <span class="inline-block bg-gray-900 text-amber-500 text-4xl px-5 py-4 rounded-full group-hover:bg-amber-500 group-hover:text-white transition duration-400">
+                    <i class="bi bi-bar-chart-fill"></i>
+                </span>
+                <h4 class="text-2xl text-white font-bold">Professional training plan</h4>
+                <p class="text-gray-400 text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sequi 
+                    neque accusamus veritatis.
+                </p>
+            </div>
+            <div class="group text-center space-y-3">
+                <span class="inline-block bg-gray-900 text-amber-500 text-4xl px-5 py-4 rounded-full group-hover:bg-amber-500 group-hover:text-white transition duration-400">
+                    <i class="bi bi-heart-pulse-fill"></i>
+                </span>
+                <h4 class="text-2xl text-white font-bold">Unique to your needs</h4>
+                <p class="text-gray-400 text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sequi 
+                    neque accusamus veritatis.
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
