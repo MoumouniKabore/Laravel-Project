c747f9694fdeae3bb6ef5a2f06cc097cae801459
Mkabore <mkabore.contact@gmail.com>
2026-06-04 12:57:59 +0000

    Ajout du pied de page du site

--- SiteWeb/salle_de_gym/resources/js/Components/Footer.vue
+++ SiteWeb/salle_de_gym/resources/js/Components/Footer.vue
@@ -2,5 +2,74 @@
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold">Footer</h1>
+    <footer>
+        <section class="bg-black/95 flex flex-wrap justify-between px-14 py-8 gap-6 text-white">
+            <div class="flex items-center gap-3">
+                <span class="bg-amber-500 px-5 py-4 rounded-full"><i class="bi bi-telephone-inbound-fill text-2xl"></i></span>
+                <p class="text-xl font-semibold">+225 00 11 22 33 44</p>
+            </div>
+            <div class="flex items-center gap-3">
+                <span class="bg-amber-500 px-5 py-4 rounded-full"><i class="bi bi-envelope-fill text-2xl"></i></span>
+                <p class="text-xl font-semibold">mkabore.exemple@gmail.com</p>
+            </div>
+            <div class="flex items-center gap-3">
+                <span class="bg-amber-500 px-5 py-4 rounded-full"><i class="bi bi-geo-alt-fill text-2xl"></i></span>
+                <p class="text-xl font-semibold">Abidjan, Côte d'ivoire</p>
+            </div>
+        </section>
+        <section class="bg-black px-14">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-1 pt-10 pb-6">
+                <div class="w-full space-y-3">
+                    <img src="/images/logo.png" alt="">
+                    <p class="text-gray-400">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore fugit, illum 
+                        repellendus voluptatibus error maxime.
+                    </p>
+                    <ul class="flex gap-4 text-white">
+                        <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+                        <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+                        <li><a href="#"><i class="bi bi-youtube"></i></a></li>
+                        <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+                        <li><a href="#"><i class="bi bi-envelope-fill"></i></a></li>
+                    </ul>
+                </div>
+                <div class="space-y-3 px-0 xl:px-18">
+                    <h3 class="capitalize text-white text-3xl font-bold">support</h3>
+                    <ul class="space-y-3 text-gray-400">
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Classe</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="space-y-3 px-0 xl:px-10">
+                    <h3 class="capitalize text-white text-3xl font-bold">useful links</h3>
+                    <ul class="space-y-3 text-gray-400">
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">My account</a></li>
+                        <li><a href="#">Subscribe</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="w-full space-y-3">
+                    <h3 class="capitalize text-white text-3xl font-bold">tips & guides</h3>
+                    <div class="border-b border-gray-600 pb-2">
+                        <p class="text-gray-300 mb-1">
+                            Physical fitness may help prevent depression, anxiety         
+                        </p>
+                        <span class="text-gray-600">3 min read | 20 Comment</span>                
+                    </div>
+                    <div>
+                        <p class="text-gray-300 mb-1">
+                            Fitness: The best exercise to lose belly fat and tone up...        
+                        </p>
+                        <span class="text-gray-600">3 min read | 20 Comment</span>                
+                    </div>
+                </div>        
+            </div>
+            <p class="text-center text-white py-4 mb-0 border-t border-gray-600">
+                Copyright <i class="bi bi-c-circle"></i> 2026 Tous droit reservé, par <span class="text-amber-500">mkabore</span>
+            </p>
+        </section>
+    </footer>
 </template>
