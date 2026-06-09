741d8206c26b43982d6108d1b52412b6d14d5210
Mkabore <mkabore.contact@gmail.com>
2026-06-09 17:12:54 +0000

    Création du composant 'price.vue' et implementation de ce composant dans la page Home

---
+++ SiteWeb/salle_de_gym/resources/js/Components/Price.vue
@@ -0,0 +1,53 @@
+<script setup>
+</script>
+
+<template>
+    <section class="bg-black/90 px-14 py-16">
+        <h4 class="uppercase text-center text-xl text-amber-500 font-semibold mb-2">Our plan</h4>
+        <h3 class="uppercase text-center text-4xl text-white font-bold">choose your pricing plan</h3>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+            <div class="border border-white p-10 text-center group hover:bg-white hover:text-black transition duration-400">
+                <h3 class="text-white text-4xl group-hover:text-black/90 font-bold mb-2">Class drop-in</h3>
+                <p class="text-amber-500 text-6xl font-bold mb-2">$ 39.0</p>
+                <p class="uppercase text-gray-300 group-hover:text-black/90 text-xl font-bold mb-8">single class</p>
+                <ul class="flex flex-col text-gray-300 group-hover:text-black/90 space-y-3 ">
+                    <i class="not-italic">Free riding</i>
+                    <i class="not-italic">Unlimited equipments</i>
+                    <i class="not-italic">Personal trainer</i>
+                    <i class="not-italic">Weight losing classes</i>
+                    <i class="not-italic">Month to mouth</i>
+                    <i class="not-italic">No time restriction</i>  
+                </ul>
+                <a href="#" class="block py-3 mt-8 bg-white/30 group-hover:bg-amber-500 text-white font-bold uppercase cursor-pointer">enroll now</a>
+            </div>
+            <div class="border border-white p-10 text-center group hover:bg-white hover:text-black transition duration-400">
+                <h3 class="text-white text-4xl group-hover:text-black/90 font-bold mb-2">12 Month unlimited</h3>
+                <p class="text-amber-500 text-6xl font-bold mb-2">$ 99.0</p>
+                <p class="uppercase text-gray-300 group-hover:text-black/90 text-xl font-bold mb-8">single class</p>
+                <ul class="flex flex-col text-gray-300 group-hover:text-black/90 space-y-3 ">
+                    <i class="not-italic">Free riding</i>
+                    <i class="not-italic">Unlimited equipments</i>
+                    <i class="not-italic">Personal trainer</i>
+                    <i class="not-italic">Weight losing classes</i>
+                    <i class="not-italic">Month to mouth</i>
+                    <i class="not-italic">No time restriction</i>  
+                </ul>
+                <a href="#" class="block py-3 mt-8 bg-white/30 group-hover:bg-amber-500 text-white font-bold uppercase cursor-pointer">enroll now</a>
+            </div>
+            <div class="border border-white p-10 text-center group hover:bg-white hover:text-black transition duration-400">
+                <h3 class="text-white text-4xl group-hover:text-black/90 font-bold mb-2">6 Month unlimited</h3>
+                <p class="text-amber-500 text-6xl font-bold mb-2">$ 59.0</p>
+                <p class="uppercase text-gray-300 group-hover:text-black/90 text-xl font-bold mb-8">single class</p>
+                <ul class="flex flex-col text-gray-300 group-hover:text-black/90 space-y-3 ">
+                    <i class="not-italic">Free riding</i>
+                    <i class="not-italic">Unlimited equipments</i>
+                    <i class="not-italic">Personal trainer</i>
+                    <i class="not-italic">Weight losing classes</i>
+                    <i class="not-italic">Month to mouth</i>
+                    <i class="not-italic">No time restriction</i>  
+                </ul>
+                <a href="#" class="block py-3 mt-8 bg-white/30 group-hover:bg-amber-500 text-white font-bold uppercase cursor-pointer">enroll now</a>
+            </div>
+        </div>
+    </section>
+</template>
