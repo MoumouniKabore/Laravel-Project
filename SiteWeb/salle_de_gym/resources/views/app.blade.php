96797e74ae6c43112b42c0f52481291e6dec6c2c
Mkabore <mkabore.contact@gmail.com>
2026-06-28 13:22:30 +0000

    Suppression du title dans le head, j'ai ajouter un title dans le template de chaque page

--- SiteWeb/salle_de_gym/resources/views/app.blade.php
+++ SiteWeb/salle_de_gym/resources/views/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Mon Application</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
         @routes
