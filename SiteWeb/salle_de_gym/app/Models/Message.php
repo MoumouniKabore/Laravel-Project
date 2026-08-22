f5111007f8a561594c599c2f609978b73828810f
Mkabore <mkabore.contact@gmail.com>
2026-08-22 11:25:18 +0000

    Création du modèle 'Message'

---
+++ SiteWeb/salle_de_gym/app/Models/Message.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'phone',
+        'email',
+        'object',
+        'message',
+        'statut',
+    ];
+
+    public function setAttribute($key, $value) {
+
+        $lowercaseFields = [
+            'firstname',
+            'lastname',
+            'email',
+            'object',
+            'message',
+            'statut',
+        ];
+
+        if (in_array($key, $lowercaseFields) && is_string($value)) {
+            $value = mb_strtolower($value);
+        }
+
+        return parent::setAttribute($key, $value);
+    }
+}
