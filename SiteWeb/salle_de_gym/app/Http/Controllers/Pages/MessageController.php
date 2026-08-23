54859f218f40a0ee720396aab7cabdf7eb720e3d
Mkabore <mkabore.contact@gmail.com>
2026-08-23 14:38:36 +0000

    Controller du module 'Message

---
+++ SiteWeb/salle_de_gym/app/Http/Controllers/Pages/MessageController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreMessageRequest;
+use App\Models\Message;
+use Inertia\Inertia;
+// use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+
+    public function all_message() {
+        $messages = Message::latest()->get();
+        return Inertia::render('Dashboard/Messages/All', [
+            'messages' => $messages
+        ]);
+    }
+
+    public function store_message(StoreMessageRequest $request) {
+        $validated = $request->validated();
+        Message::create($validated);
+        return redirect()->back();
+    }
+
+    public function show_message(Message $message){
+        return Inertia::render('Dashboard/Messages/View', [
+            'message' => $message
+        ]);
+    }
+
+    public function delete_message(Message $message) {
+        $message->delete();
+        return redirect()->route('all.message');
+    }
+}
