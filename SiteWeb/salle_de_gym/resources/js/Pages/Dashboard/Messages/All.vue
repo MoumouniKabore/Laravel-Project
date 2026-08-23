041bf26d66c5dedb927c0495c223d5a39a259a5c
Mkabore <mkabore.contact@gmail.com>
2026-08-23 14:33:46 +0000

    Ces pages permettent de voir tous les messages reçu et aussi voir les messages un par un

--- SiteWeb/salle_de_gym/resources/js/Pages/Dashboard/Messages/All.vue
+++ SiteWeb/salle_de_gym/resources/js/Pages/Dashboard/Messages/All.vue
@@ -1,7 +1,43 @@
 <script setup>
 
     import DashboardLayout from '../../../Layouts/DashboardLayout.vue'
-    import { Head, Link } from '@inertiajs/vue3'
+    import { Head, Link, router } from '@inertiajs/vue3'
+    import { ref } from 'vue'
+
+    defineProps({
+        messages: Array,
+    })
+
+    const capitalize = (text) => {
+        return text.charAt(0).toUpperCase() + text.slice(1)
+    }
+
+    const truncate = (text, length) => {
+        return text.length > length ? text.substring(0, length) + '...' : text
+    }
+
+    const showmodal = ref(false)
+    const selectedMessage = ref(null)
+
+    const openModal = (message) => {
+        selectedMessage.value = message
+        showmodal.value = true
+    }
+
+    const closeModal = () => {
+        selectedMessage.value = null
+        showmodal.value = false
+    }
+
+    const delete_message = () => {
+        router.delete(
+            route('delete.message', selectedMessage.value.id), {
+                onSuccess: () => {
+                    closeModal()
+                }
+            }
+        )
+    }
 
 </script>
 
@@ -17,42 +53,59 @@
                     </h2>
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    <div class="bg-white rounded-2xl p-5 hover:shadow-xl transition">
+
+                    <div v-for="message in messages" :key="message.id" class="bg-white rounded-2xl p-5 hover:shadow-xl transition">
                         <div class="flex items-center gap-4">
                             <div>
-                                <h3 class="font-bold text-md text-gray-800">
-                                    Kouassi Jean
+                                <h3 class="font-bold text-md text-gray-800 capitalize">
+                                    {{ message.firstname }} {{ message.lastname }}
                                 </h3>
                             </div>
                         </div>
                         <div class="mt-4 space-y-2 text-sm">
                             <div class="flex items-center gap-2">
                                 <i class="bi bi-telephone text-gray-500"></i>
-                                <span>+225 07 00 00 00 00</span>
+                                <span>{{ message.phone }}</span>
                             </div>
                             <div class="flex items-start gap-2">
                                 <i class="bi bi-envelope text-gray-500"></i>
-                                <span>exemple@gmail.com</span>
+                                <span>{{ message.email }}</span>
                             </div>
                             <div class="flex items-start gap-2">
                                 <i class="bi bi-box text-gray-500"></i>
-                                <span>Rendez-vous</span>
+                                <span>{{ capitalize(truncate(message.object, 20)) }}</span>
                             </div>
                             <div class="flex items-center gap-2">
                                 <i class="bi bi-chat-right-dots text-gray-500"></i>
-                                <span>Lorem ipsum dolor sit amet consectetur adipisicing...</span>
+                                <span>{{ capitalize(truncate(message.message, 40)) }}</span>
                             </div>
                         </div>
                         
                         <div class="flex justify-end gap-2 mt-5 pt-4 border-t border-gray-400 text-sm">
-                            <Link :href="route('messages-view')" class="p-2 text-white rounded-lg bg-green-400 hover:bg-green-500 transition" title="Voir détail">
+                            <Link :href="route('show.message', message.id)" class="p-2 text-white rounded-lg bg-green-400 hover:bg-green-500 transition" title="Voir détail">
                                 <i class="bi bi-eye"></i>
                             </Link>
-                            <a href="#" class="p-2 text-white rounded-lg bg-red-600 hover:bg-red-700 transition" title="Supprimer">
+                            <button @click="openModal(message)" class="p-2 text-white rounded-lg bg-red-600 hover:bg-red-700 transition" title="Supprimer">
                                 <i class="bi bi-trash3"></i>
-                            </a>
+                            </button>
                         </div>
                     </div>
+
+                    <!-- Modale se suppression -->
+                    <div
+                        v-if="showmodal"
+                        class="fixed inset-0 bg-black/50 flex justify-center items-center"
+                    >
+                        <div class="bg-white p-6 rounded-lg w-90">
+                            <h2 class="font-bold">Confirmer la suppression</h2>
+                            <p class="py-4">Voulez-vous vraiment supprimer le message de <strong>{{ selectedMessage?.firstname }}</strong> ?</p>
+                            <div class="flex justify-end gap-3 border-t border-gray-300 pt-2">
+                                <button @click="closeModal" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600">Annuler</button>
+                                <button @click="delete_message" class="p-2 text-white rounded-lg bg-red-600 hover:bg-red-700 transition">Supprimer</button>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
--- SiteWeb/salle_de_gym/resources/js/Pages/Dashboard/Messages/View.vue
+++ SiteWeb/salle_de_gym/resources/js/Pages/Dashboard/Messages/View.vue
@@ -1,7 +1,35 @@
 <script setup>
 
     import DashboardLayout from '../../../Layouts/DashboardLayout.vue'
-    import { Head, Link } from '@inertiajs/vue3'
+    import { Head, Link, router } from '@inertiajs/vue3'
+    import { ref } from 'vue'
+
+    defineProps({
+        message: Object,
+    })
+
+    const showmodal = ref(false)
+    const selectedMessage = ref(null)
+
+    const openModal = (message) => {
+        selectedMessage.value = message
+        showmodal.value = true
+    }
+
+    const closeModal = () => {
+        selectedMessage.value = null
+        showmodal.value = false
+    }
+
+    const delete_message = () => {
+        router.delete(
+            route('delete.message', selectedMessage.value.id), {
+                onSuccess: () => {
+                    closeModal()
+                }
+            }
+        )
+    }
 
 </script>
 
@@ -13,35 +41,36 @@
             <!-- Header -->
             <div class="bg-white rounded-xl shadow-sm flex items-center justify-between px-6 py-4 mb-5">
                 <h2 class="text-xl font-bold text-gray-800">
-                    Messages - Kouassi Marc
+                    Messages de <span class="capitalize">{{ message.firstname }} {{ message.lastname }}</span>
                 </h2>
             </div>
         </div>
         <div class="max-w-xl mx-auto bg-white rounded-xl shadow-lg overflow-hidden mt-10">
             
-            <div class="bg-blue-600 p-3">
-                <h1 class="text-xl font-bold text-white">
-                    Kouassi Marc
+            <div class="flex justify-between items-center bg-blue-600 p-3">
+                <h1 class="text-xl font-bold text-white capitalize">
+                    {{ message.firstname }} {{ message.lastname }}
                 </h1>
+                <span class="capitalize">non lu</span>
             </div>
-            <div class="py-6">
+            <div class="p-6">
                 <div class="flex items-centrer justify-around">
-                    <div class="space-y-4 text-sm">
+                    <div class="space-y-2 text-sm">
                         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
                             <div>
                                 <label class="block font-semibold text-gray-500">
-                                    Nom
+                                    Nom Complet
                                 </label>
                                 <p class="text-gray-800">
-                                    Kouassi Marc
+                                    {{ message.firstname }} {{ message.lastname }}
                                 </p>
                             </div>
                             <div>
                                 <label class="block font-semibold text-gray-500">
-                                    Prénom
+                                    Téléphone
                                 </label>
                                 <p class="text-gray-800">
-                                    +225 07 00 00 00 00
+                                    {{ message.phone }}
                                 </p>
                             </div>
                             <div>
@@ -49,7 +78,7 @@
                                     Email
                                 </label>
                                 <p class="text-gray-800">
-                                    exemple@gmail.com
+                                    {{ message.email }}
                                 </p>
                             </div>
                             <div>
@@ -57,7 +86,7 @@
                                     Objet
                                 </label>
                                 <p class="text-gray-800">
-                                    Rendez-vous
+                                    {{ message.object }}
                                 </p>
                             </div>                    
                         </div>
@@ -65,20 +94,35 @@
                             <label class="block font-semibold text-gray-500">
                                 Messages
                             </label>
-                            <p class="text-gray-800 w-80">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem hic nihil quibusdam quis. Neque tempore id autem dele.
+                            <p class="text-gray-800">
+                                {{ message.message }}
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="mt-6 flex flex-wrap text-sm font-bold gap-2 justify-end border-t border-gray-400 pt-6">
-                    <Link :href="route('messages-all')" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600">
+                <div class="mt-6 flex flex-wrap text-sm font-bold gap-2 justify-end border-t border-gray-400 pt-4">
+                    <Link :href="route('all.message')" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600">
                         Retour
                     </Link>
-                    <a href="#" class="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700">
+                    <button @click="openModal(message)" class="p-2 text-white rounded-lg bg-red-600 hover:bg-red-700 transition">
                         Supprimer
-                    </a>
+                    </button>
+                </div>
+
+                <!-- Modale se suppression -->
+                <div
+                    v-if="showmodal"
+                    class="fixed inset-0 bg-black/50 flex justify-center items-center"
+                >
+                    <div class="bg-white p-6 rounded-lg w-90">
+                        <h2 class="font-bold">Confirmer la suppression</h2>
+                        <p class="py-4">Voulez-vous vraiment supprimer le message de <strong>{{ selectedMessage?.firstname }}</strong> ?</p>
+                        <div class="flex justify-end gap-3 border-t border-gray-300 pt-2">
+                            <button @click="closeModal" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600">Annuler</button>
+                            <button @click="delete_message" class="p-2 text-white rounded-lg bg-red-600 hover:bg-red-700 transition">Supprimer</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
