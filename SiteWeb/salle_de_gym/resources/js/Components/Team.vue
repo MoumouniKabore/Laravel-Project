bc959f7d5f9d02508e0b72ae0714737b3a6314a4
Mkabore <mkabore.contact@gmail.com>
2026-08-25 18:34:02 +0000

    Modification du composant 'Team' pour éviter la répétition du formulaire Appointment

--- SiteWeb/salle_de_gym/resources/js/Components/Team.vue
+++ SiteWeb/salle_de_gym/resources/js/Components/Team.vue
@@ -1,52 +1,13 @@
 <script setup>
-    import { ref } from 'vue'
-    import { onMounted, nextTick } from 'vue'
 
-    // Team Modal Appointment
+    import { ref } from 'vue'
     const showModal = ref(false)
 
-    // Tream Section
-    onMounted(async () => {
-        await nextTick()
-        
-        new window.Swiper('.teamSwiper', {
-            loop: true,
-            speed: 1000, // Un peu plus rapide (1s) pour que ce soit fluide
-            autoplay: {
-                delay: 4000,
-                disableOnInteraction: false,
-            },
-            // --- AJOUTS POUR LA LISTE D'ÉQUIPE ---
-            spaceBetween: 10, // Espace entre les cartes en pixels
-            slidesPerView: 1, // Par défaut (mobile) : 1 seule carte
-            breakpoints: {
-                // Quand l'écran est >= 640px (Tablette)
-                640: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
-                },
-                // Quand l'écran est >= 1024px (PC Portable)
-                1024: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
-                },
-            },
-            // ------------------------------------
-            navigation: {
-                nextEl: '.custom-next',
-                prevEl: '.custom-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        })
-    })
 </script>
 
 <template>
-    <section class="bg-black/90 px-10 md:px-25 pt-16 pb-20">
-        <div class="flex justify-between items-center mb-10">
+    <section class="mb-10">
+        <div class="flex justify-between items-center">
             <div>
                 <h4 class="uppercase text-xl text-amber-500 font-semibold">our team</h4>
                 <h3 class="uppercase text-3xl text-white font-bold">train with experts</h3>
@@ -54,65 +15,6 @@
             <div>
                 <span @click="showModal = true" class="uppercase text-white border-2 border-amber-500 hover:bg-amber-500 py-3 px-10 duration-400 cursor-pointer">appointment</span>
             </div>
-        </div>
-        <div class="swiper teamSwiper cursor-grab pb-14">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide group relative overflow-hidden">
-                    <div class="w-full h-full">
-                        <img src="/images/team/team-1.jpg" class="w-full h-110 object-cover" />
-                    </div>
-                    <div class="w-full h-30 absolute bottom-0 bg-zinc-950 border-t-5 border-amber-500 flex flex-col justify-center items-center transform translate-y-full transition-transform duration-500 ease-out group-hover:translate-y-0">
-                        <h3 class="capitalize text-2xl font-semibold text-white">athart rachel</h3>
-                        <p class="uppercase text-sm font-medium text-gray-400">gym trainer</p>
-                    </div>
-                </div>
-                <div class="swiper-slide group relative overflow-hidden">
-                    <div class="w-full h-full">
-                        <img src="/images/team/team-2.jpg" class="w-full h-110 object-cover" />
-                    </div>
-                    <div class="w-full h-30 absolute bottom-0 bg-zinc-950 border-t-5 border-amber-500 flex flex-col justify-center items-center transform translate-y-full transition-transform duration-500 ease-out group-hover:translate-y-0">
-                        <h3 class="capitalize text-2xl font-semibold text-white">athart rachel</h3>
-                        <p class="uppercase text-sm font-medium text-gray-400">gym trainer</p>
-                    </div>
-                </div>
-                <div class="swiper-slide group relative overflow-hidden">
-                    <div class="w-full h-full">
-                        <img src="/images/team/team-3.jpg" class="w-full h-110 object-cover" />
-                    </div>
-                    <div class="w-full h-30 absolute bottom-0 bg-zinc-950 border-t-5 border-amber-500 flex flex-col justify-center items-center transform translate-y-full transition-transform duration-500 ease-out group-hover:translate-y-0">
-                        <h3 class="capitalize text-2xl font-semibold text-white">athart rachel</h3>
-                        <p class="uppercase text-sm font-medium text-gray-400">gym trainer</p>
-                    </div>
-                </div>
-                <div class="swiper-slide group relative overflow-hidden">
-                    <div class="w-full h-full">
-                        <img src="/images/team/team-4.jpg" class="w-full h-110 object-cover" />
-                    </div>
-                    <div class="w-full h-30 absolute bottom-0 bg-zinc-950 border-t-5 border-amber-500 flex flex-col justify-center items-center transform translate-y-full transition-transform duration-500 ease-out group-hover:translate-y-0">
-                        <h3 class="capitalize text-2xl font-semibold text-white">athart rachel</h3>
-                        <p class="uppercase text-sm font-medium text-gray-400">gym trainer</p>
-                    </div>
-                </div>
-                <div class="swiper-slide group relative overflow-hidden">
-                    <div class="w-full h-full">
-                        <img src="/images/team/team-5.jpg" class="w-full h-110 object-cover" />
-                    </div>
-                    <div class="w-full h-30 absolute bottom-0 bg-zinc-950 border-t-5 border-amber-500 flex flex-col justify-center items-center transform translate-y-full transition-transform duration-500 ease-out group-hover:translate-y-0">
-                        <h3 class="capitalize text-2xl font-semibold text-white">athart rachel</h3>
-                        <p class="uppercase text-sm font-medium text-gray-400">gym trainer</p>
-                    </div>
-                </div>
-                <div class="swiper-slide group relative overflow-hidden">
-                    <div class="w-full h-full">
-                        <img src="/images/team/team-6.jpg" class="w-full h-110 object-cover" />
-                    </div>
-                    <div class="w-full h-30 absolute bottom-0 bg-zinc-950 border-t-5 border-amber-500 flex flex-col justify-center items-center transform translate-y-full transition-transform duration-500 ease-out group-hover:translate-y-0">
-                        <h3 class="capitalize text-2xl font-semibold text-white">athart rachel</h3>
-                        <p class="uppercase text-sm font-medium text-gray-400">gym trainer</p>
-                    </div>
-                </div>
-            </div>
-            <div class="swiper-pagination mt-20"></div>
         </div>
         <Teleport to="body">
             <Transition
@@ -123,9 +25,10 @@
                 leave-from-class="opacity-100"
                 leave-to-class="opacity-0"
             >
-                <div @click.self="showModal = false" v-if="showModal"
+                <div
+                    v-if="showModal"
                     class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4"
-                    
+                    @click.self="showModal = false"
                 >
                     <div class="bg-white rounded-lg shadow-xl w-full max-w-lg p-6">
                         <!-- Header -->
@@ -141,6 +44,15 @@
 
                         <!-- Formulaire -->
                         <form class="pb-6 space-y-4 overflow-y-auto pr-3 max-h-[70vh]">
+                            <div>
+                                <label for="image" class="block mb-2 text-sm font-medium text-gray-700 capitalize">
+                                    your picture
+                                </label>
+                                <input type="file" id="image" name="image" accept="image/*"
+                                    class="block w-full text-sm text-gray-600 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0
+                                        file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 cursor-pointer"
+                                >
+                            </div>
                             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 <div>
                                     <label for="firstname" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
