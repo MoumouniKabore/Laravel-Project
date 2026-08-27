dfa991124f6a2d6770b96c285573e9ae4f3cbd56
Mkabore <mkabore.contact@gmail.com>
2026-08-27 15:36:53 +0000

    Correction de la route des avis client et ajout du compteur de message non lu

--- SiteWeb/salle_de_gym/resources/js/Layouts/DashboardLayout.vue
+++ SiteWeb/salle_de_gym/resources/js/Layouts/DashboardLayout.vue
@@ -43,9 +43,10 @@
                     <span class="text-xl"><i class="bi bi-journal-check"></i></span>
                     <span>Réservations</span>
                 </Link>
-                <Link :href="route('testimonies-all')" class="flex items-center gap-3 px-4 py-2 rounded-xl hover:bg-blue-800 transition">
+                <Link :href="route('all.testimonie')" class="flex items-center gap-3 px-4 py-2 rounded-xl hover:bg-blue-800 transition">
                     <span class="text-xl"><i class="bi bi-chat-square-quote"></i></span>
                     <span>Avis Clients</span>
+                    <span v-if="page.props.testimonie_non_lu != 0" class="text-sm font-bold rounded-full px-3 py-1 bg-green-400">{{ page.props.testimonie_non_lu }}</span>
                 </Link>
                 <Link :href="route('all.message')" class="flex items-center gap-3 px-4 py-2 rounded-xl hover:bg-blue-800 transition">
                     <span class="text-xl"><i class="bi bi-chat-right-dots"></i></span>
