22736cc6b7fa61b9afb70d76990a54294bf46a58
Mkabore <mkabore.contact@gmail.com>
2026-08-27 15:55:59 +0000

    Le modele pour les avis clients

---
+++ SiteWeb/salle_de_gym/app/Models/Testimonie.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Testimonie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'phone',
+        'star',
+        'opinion',
+        'photo',
+        'statut',
+        'publish',
+    ];
+
+    public function setAttribute($key, $value) {
+
+        $lowercaseFields = [
+            'firstname',
+            'lastname',
+            'email',
+            'opinion',
+            'statut',
+            'publish',
+        ];
+
+        if (in_array($key, $lowercaseFields) && is_string($value)) {
+            $value = mb_strtolower($value);
+        }
+
+        return parent::setAttribute($key, $value);
+    }
+}
