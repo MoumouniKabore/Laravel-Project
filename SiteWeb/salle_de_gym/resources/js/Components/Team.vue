fecbcdfd3c1e8e2ac702ec4bc11fb4e89091bb5a
Mkabore <mkabore.contact@gmail.com>
2026-08-29 18:06:56 +0000

    Ajustement du formulaire pour permettre l'envoie des données et gestion des erreur d'envoie, ajout du script js permettant l'envoie vers la route

--- SiteWeb/salle_de_gym/resources/js/Components/Team.vue
+++ SiteWeb/salle_de_gym/resources/js/Components/Team.vue
@@ -1,7 +1,29 @@
 <script setup>
 
     import { ref } from 'vue'
+    import { useForm } from '@inertiajs/vue3'
+
     const showModal = ref(false)
+
+    const form = useForm({
+        firstname: '',
+        lastname: '',
+        phone: '',
+        address: '',
+        message: '',
+        photo: null,
+    })
+
+    const submit = () => {
+        form.post(route('store.appointment'), {
+            forceFormData: true,
+            onSuccess: () => {
+                form.reset(),
+                showModal.value = false
+            }
+        });
+    }
+
 
 </script>
 
@@ -43,72 +65,104 @@
                         </div>
 
                         <!-- Formulaire -->
-                        <form class="pb-6 space-y-4 overflow-y-auto pr-3 max-h-[70vh]">
+                        <form @submit.prevent="submit" class="pb-6 space-y-4 overflow-y-auto pr-3 max-h-[70vh]">
                             <div>
-                                <label for="image" class="block mb-2 text-sm font-medium text-gray-700 capitalize">
-                                    your picture
-                                </label>
-                                <input type="file" id="image" name="image" accept="image/*"
-                                    class="block w-full text-sm text-gray-600 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0
-                                        file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 cursor-pointer"
-                                >
+                                <div>
+                                    <label for="photo" class="block mb-2 text-sm font-medium text-gray-700 capitalize">
+                                        your picture
+                                    </label>
+                                    <input type="file" id="photo" name="photo" accept="image/*" @input="form.photo = $event.target.files[0]"
+                                        class="block w-full text-sm text-gray-600 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0
+                                            file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 cursor-pointer"
+                                    >
+                                </div>
+                                <p v-if="form.errors.photo" class="text-red-500 text-sm mt-1">
+                                    {{ form.errors.photo }}
+                                </p>
                             </div>
                             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 <div>
-                                    <label for="firstname" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
-                                        first name
-                                    </label>
-                                    <input type="text" id="firstname" name="firstname" placeholder="Kabore"
-                                        class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
-                                        required
-                                    >
+                                    <div>
+                                        <label for="firstname" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
+                                            first name
+                                        </label>
+                                        <input type="text" id="firstname" name="firstname" placeholder="Kabore" v-model="form.firstname"
+                                            class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
+                                            required
+                                        >
+                                    </div>
+                                    <p v-if="form.errors.firstname" class="text-red-500 text-sm mt-1">
+                                        {{ form.errors.firstname }}
+                                    </p>
                                 </div>
                                 <div>
-                                    <label for="lastname" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
-                                        last name
-                                    </label>
-                                    <input type="text" id="lastname" name="lastname" placeholder="Moumouni"
-                                        class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
-                                        required
-                                    >
-                                </div>                        
+                                    <div>
+                                        <label for="lastname" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
+                                            last name
+                                        </label>
+                                        <input type="text" id="lastname" name="lastname" placeholder="Moumouni" v-model="form.lastname"
+                                            class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
+                                            required
+                                        >
+                                    </div>
+                                    <p v-if="form.errors.lastname" class="text-red-500 text-sm mt-1">
+                                        {{ form.errors.lastname }}
+                                    </p>
+                                </div>                       
                             </div>
                             <div class="grid sm:grid-cols-2 gap-4">
                                 <div>
-                                    <label for="phone" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
-                                        phone number
-                                    </label>
-                                    <input type="text" id="phone" name="phone" placeholder="Ex: +225 00 07 00 00 00"
-                                        class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
-                                        required
-                                    >
+                                    <div>
+                                        <label for="phone" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
+                                            phone number
+                                        </label>
+                                        <input type="text" id="phone" name="phone" placeholder="Ex: +225 00 07 00 00 00" v-model="form.phone"
+                                            class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
+                                            required
+                                        >
+                                    </div>
+                                    <p v-if="form.errors.phone" class="text-red-500 text-sm mt-1">
+                                        {{ form.errors.phone }}
+                                    </p>
                                 </div>
                                 <div>
-                                    <label for="address" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
-                                        your address
-                                    </label>
-                                    <input type="text" id="address" name="address" placeholder="Ex: Abidjan"
-                                        class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
-                                        required
-                                    >
+                                    <div>
+                                        <label for="address" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
+                                            your address
+                                        </label>
+                                        <input type="text" id="address" name="address" placeholder="Ex: Abidjan" v-model="form.address"
+                                            class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
+                                            required
+                                        >
+                                    </div>
+                                    <p v-if="form.errors.address" class="text-red-500 text-sm mt-1">
+                                        {{ form.errors.address }}
+                                    </p>
                                 </div>                        
                             </div>
                             <div>
-                                <label for="message" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
-                                    your message
-                                </label>
-                                <textarea name="message" id="message" rows="4"
-                                    class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
-                                    placeholder="What's your message ?"
-                                    required
-                                ></textarea>
+                                <div>
+                                    <label for="message" class="block text-sm mb-2 font-medium text-gray-700 capitalize">
+                                        your message
+                                    </label>
+                                    <textarea name="message" id="message" rows="4" v-model="form.message"
+                                        class="w-full px-4 py-1 border border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none"
+                                        placeholder="What's your message ?"
+                                        required
+                                    ></textarea>
+                                </div>
+                                <p v-if="form.errors.message" class="text-red-500 text-sm mt-1">
+                                    {{ form.errors.message }}
+                                </p>
                             </div>
                             <div class="flex justify-end gap-2">
                                 <button type="button" @click="showModal = false" class="px-4 py-2 font-semibold capitalize border border-gray-400 cursor-pointer rounded hover:bg-red-600 hover:border-red-600 hover:text-white transition duration-400">
                                     close
                                 </button>
-                                <button type="submit" class="px-4 py-2 bg-amber-500 hover:bg-amber-600 text-white font-semibold capitalize rounded cursor-pointer transition duration-400">
-                                    send
+                                <button type="submit" :disabled="form.processing"
+                                    class="px-4 py-2 bg-amber-500 hover:bg-amber-600 text-white font-semibold capitalize rounded cursor-pointer transition duration-400"
+                                >
+                                    {{ form.processing ? 'Saving...' : 'Send' }}
                                 </button>
                             </div>
                         </form>
