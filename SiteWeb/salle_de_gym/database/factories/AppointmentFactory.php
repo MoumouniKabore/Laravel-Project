6e2da33a36f1a721d24351f5badc282a8c10ead2
Mkabore <mkabore.contact@gmail.com>
2026-08-30 12:20:30 +0000

    Factorie du model Appointment, permet de définir les données fictif à ajouter

---
+++ SiteWeb/salle_de_gym/database/factories/AppointmentFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Appointment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @extends Factory<Appointment>
+ */
+class AppointmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $images = [
+            'appointment1.jpg',
+            'appointment2.jpg',
+            'appointment3.jpg',
+            'appointment4.jpg',
+            'appointment5.jpg',
+            'appointment6.jpg',
+            'appointment7.jpg',
+            'appointment8.jpg',
+            'appointment9.jpg',
+            'appointment10.jpg',
+        ];
+        
+        $image = fake()->unique()->randomElement($images);
+        $source = database_path('seeders/imagesAppointments/'.$image);
+        $filename = uniqid().'_'.$image;
+        Storage::disk('public')->put('appointment/'.$filename, file_get_contents($source));
+
+        return [
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+            'message' => fake()->paragraphs(fake()->numberBetween(1, 2), true),
+            'statut' => fake()->randomElement(['lu', 'non lu']),
+            'photo' => 'appointment/'.$filename,
+        ];
+    }
+}
