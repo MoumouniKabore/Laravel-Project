f6afd88ef214deb10c7300f2058481f8d4b41aed
Mkabore <mkabore.contact@gmail.com>
2026-08-30 12:02:22 +0000

    Controller du module 'Appointment', permettant la récupération de toutes les réservations, enregistrer une nouvelle réservation, récupérer une réservation en particulier et envoyer les informations, supprimer une réservation et aussi changer le statut de la reservation en est 'lu' ou 'non lu'

---
+++ SiteWeb/salle_de_gym/app/Http/Controllers/Dash/AppointmentController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Dash;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAppointmentRequest;
+use App\Models\Appointment;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
+// use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{    
+
+    public function all_appointment() {
+        $appointments = Appointment::latest()->get();
+        return Inertia::render('Dashboard/Appointments/All', [
+            'appointments' => $appointments
+        ]);
+    }
+
+    public function store_appointment(StoreAppointmentRequest $request) {
+        $validated = $request->validated();
+        if ($request->hasfile('photo')) {
+            $image = $request->file('photo');
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            $path = $image->storeAs('appointment', $filename, 'public');
+            $validated['photo'] = $path;
+        }
+        Appointment::create($validated);
+        return redirect()->back();
+    }
+
+    public function show_appointment(Appointment $appointment){
+        return Inertia::render('Dashboard/Appointments/View', [
+            'appointment' => $appointment
+        ]);
+    }
+
+    public function delete_appointment(Appointment $appointment) {
+        if (!empty($appointment->photo) && Storage::disk('public')->exists($appointment->photo)) {
+            Storage::disk('public')->delete($appointment->photo);
+        }
+        $appointment->delete();
+        return redirect()->route('all.appointment');
+    }
+
+    public function change_statut(Appointment $appointment) {
+        $appointment->statut = $appointment->statut === 'lu' ? 'non lu' : 'lu';
+        $appointment->save();
+        return back();
+    }
+}
