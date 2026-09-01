c01842886c636928cffa19ed10f343733dd65d49
Mkabore <mkabore.contact@gmail.com>
2026-09-01 17:59:30 +0000

    Factory du model Team, génération de donnée fictf

---
+++ SiteWeb/salle_de_gym/database/factories/TeamFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Team;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @extends Factory<Team>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $images = [
+            'team1.jpg',
+            'team2.jpg',
+            'team3.jpg',
+            'team4.jpg',
+            'team5.jpg',
+            'team6.jpg',
+        ];
+        
+        $image = fake()->unique()->randomElement($images);
+        $source = database_path('seeders/imagesTeams/'.$image);
+        $filename = uniqid().'_'.$image;
+        Storage::disk('public')->put('team/'.$filename, file_get_contents($source));
+
+        return [
+            'fullname' => fake()->name(),
+            'fonction' => fake()->randomElement(['coach principal', 'coach sportif', 'instructeur fitness', 'éducateur sportif', 'moniteur plateau']),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+            'email' => fake()->unique()->safeEmail(),
+            'photo' => 'team/'.$filename,
+        ];
+    }
+}
