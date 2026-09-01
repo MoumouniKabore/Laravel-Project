33a6323a9d0dd833cbbfa616f480979280e739ad
Mkabore <mkabore.contact@gmail.com>
2026-09-01 18:52:54 +0000

    Correction de la route 'teams-all' par 'teams.index' de la sidebar

--- SiteWeb/salle_de_gym/resources/js/Layouts/DashboardLayout.vue
+++ SiteWeb/salle_de_gym/resources/js/Layouts/DashboardLayout.vue
@@ -23,7 +23,7 @@
             class="fixed md:static inset-y-0 left-0 z-50 w-65 bg-red-500 text-white transform transition-transform duration-300 ease-in-out md:translate-x-0"
             :class="isMenuOpen ? 'translate-x-0' : '-translate-x-full'"
         >
-            <div class="flex items-center justify-between px-4 py-4 border-b border-gray-500">
+            <div class="flex items-center md:justify-center justify-between px-4 py-4 border-b border-gray-200">
                 <h1 class="text-xl font-bold">Dashboard - Gym</h1>
                 <button @click="closeMenu" class="md:hidden hover:text-gray-300 cursor-pointer">
                     <i class="bi bi-x-lg"></i>
@@ -35,7 +35,7 @@
                     <span class="text-xl"><i class="bi bi-house-check"></i></span>
                     <span>Home</span>
                 </Link>
-                <Link :href="route('teams-all')" :class="['flex items-center gap-3 px-4 py-2 rounded-xl hover:bg-red-600 transition', route().current('*.team') ? 'bg-red-600 transition' : '']">
+                <Link :href="route('teams.index')" :class="['flex items-center gap-3 px-4 py-2 rounded-xl hover:bg-red-600 transition', route().current('teams.*') ? 'bg-red-600 transition' : '']">
                     <span class="text-xl"><i class="bi bi-person-gear"></i></span>
                     <span>Équipes</span>
                 </Link>
