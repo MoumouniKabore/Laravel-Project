1837eaa88a5a0d2c5d836d4b7efe0872f11fc9c4
Mkabore <mkabore.contact@gmail.com>
2026-09-05 12:15:48 +0000

    Ajout de requete permettant de récupérer les données des models Team et Testimonie pour affichage sur les pages 'home', 'about' et 'team'

--- SiteWeb/salle_de_gym/app/Http/Controllers/Pages/PagesController.php
+++ SiteWeb/salle_de_gym/app/Http/Controllers/Pages/PagesController.php
@@ -3,17 +3,27 @@
 namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
+use App\Models\Team;
+use App\Models\Testimonie;
 use Inertia\Inertia;
 // use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function home(){
-        return Inertia::render('Site/Home');
+        $teams = Team::where('publish', 'publier')->get();
+        return Inertia::render('Site/Home', [
+            'teams' => $teams,
+        ]);
     }
 
     public function about(){
-        return Inertia::render('Site/About');
+        $teams = Team::where('publish', 'publier')->get();
+        $testimonies = Testimonie::where('publish', 'publier')->get();
+        return Inertia::render('Site/About', [
+            'teams' => $teams,
+            'testimonies' => $testimonies,
+        ]);
     }
 
     public function session(){
@@ -25,7 +35,10 @@ class PagesController extends Controller
     }
 
     public function team(){
-        return Inertia::render('Site/Team');
+        $teams = Team::where('publish', 'publier')->get();
+        return Inertia::render('Site/Team', [
+            'teams' => $teams,
+        ]);
     }
 
     public function gallery(){
